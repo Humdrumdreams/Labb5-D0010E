c9f3f0f3dab4a408dc7c41cbc5a07c73f14a5db1
Segway-tech <83791518+Segway-tech@users.noreply.github.com>
2023-02-28 14:26:25 +0100

    Update CalculatorButton.java

--- CalculatorButton.java
+++ CalculatorButton.java
@@ -1,41 +1,33 @@
-package labb5.Buttons;
+package labb5;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 
-public abstract class CalculatorButton extends JButton implements ActionListener{
+public abstract class CalculatorButton extends JButton 
+implements ActionListener{
 	
 	
-	public CalculatorButton(String s, ) {
-		
+	private State state;
+
+	public CalculatorButton(String s , State state) {
+		super(s);
+		this.setFont(new Font("monspaced", Font.PLAIN, 20));
+		this.setForeground(Color.BLACK);
+		this.setBackground(Color.WHITE);
+		this.addActionListener(actionListener);
+		this.state = state;
 	}
-	
-	
-	
-	abstract public void transition();
-	
-	
-	switch(num) {
-		case 1: //Input 1
-		
-		case 2: //OpReady
-			
-		case 3: //Input 3
-			
-		case 4: //HasResult
-			
-			
-	
-	
-	}
-	
-			
 	
 	public void actionPerformed(ActionEvent e) {
-		//count ++;
-		//label.setText("Number of clicks: " + count);
-		
+		this.transition();
 	}
+	
+		abstract void transition();
+		
+	
+	
 }
