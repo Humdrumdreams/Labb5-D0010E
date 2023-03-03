df740ecdcdd911092de4bc52424acc23df556aea
Segway-tech <83791518+Segway-tech@users.noreply.github.com>
2023-03-03 13:25:56 +0100

    Update EqualsButton.java

--- EqualsButton.java
+++ EqualsButton.java
@@ -1,5 +1,32 @@
-package labb5.Buttons;
+package labb5;
 
 public class EqualsButton extends CalculatorButton{
-
+	EqualsButton(String label, Situation situation){
+		super(label, situation);
+	}
+	public void transition() {
+		switch(situation.state) {
+		case Input1:
+		case OpReady:
+		case HasResult:
+			break;
+		case Input2:
+			int rightOperand = Integer.parseInt(situation.display.getText());
+			
+			int result = 0;
+			
+		try {
+			result = situation.binaryOperator.operator.applyAsInt(situation.leftOperand, rightOperand);
+			
+		}
+		catch(ArithmeticException e){
+			situation.display.setText("Error");	
+		}
+		situation.binaryOperator.setActive(false);
+		situation.binaryOperator = null;
+		situation.state = State.HasResult;
+		break;
+		
+		}
+	}
 }
